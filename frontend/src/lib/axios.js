a9ee18518106d3a988533ef2b2633ba0ce322208
mrittikaudayakumar-88 <mrittikaudayakumar@gmail.com>
2026-02-26 16:18:51 +0530

    prepared for the development

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:5001/api/notes",
-  headers: {
-    "Content-Type": "application/json",
-  },
- 
+  baseURL: BASE_URL,
+
 });
 
 export default api;
